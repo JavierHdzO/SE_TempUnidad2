4a6982cd13879dccf8f87069e812e9854067820b
JavierHdzO <fco_hernandez1705@hotmail.com>
2022-03-28 23:33:53 -0600

    cm2 280322F AlgGenSphere

---
+++ Programas/GeneticoParaBinarios/mainSphere.py
@@ -0,0 +1,130 @@
+import random as rnd
+
+
+def calc_FO_Sphere(indv):
+    cont = 0
+    for i in indv:
+        cont += i**2
+    return cont
+
+#m = numero de genes
+tot_genes = 1000
+
+#n  = numero de vectores
+tot_individuos = 120  #numero de individuos
+
+#Poblacion Inicial
+
+poblacion = []
+for i in range(tot_individuos):
+
+
+    vector = [ float(rnd.randrange(-10,10))  for i in range(tot_genes)]
+    print(vector)
+    ##               vector , FO
+    poblacion.append([vector, calc_FO_Sphere(vector)])
+
+#print("Poblacion Inicial: ")
+#for indv in poblacion:
+#    print(indv)
+
+it = 1
+mejorActual = 99999999
+while it<=1000:
+    print("Iteracion : ", it)
+    it+=1
+
+    padres = []
+    tot_padres = 20
+
+
+    #    Reverse es false, porque fO = 0 siendo que los mejores son los que se acerquen a 0
+    poblacion.sort(key= lambda x:x[1], reverse=False)
+
+
+    #    Para sphere es mejor los menores
+    if poblacion[0][1] <= mejorActual:
+        mejorActual = poblacion[0][1]
+
+
+
+    #Me quedo con los MejoresPadres
+    poblacion = poblacion[0:tot_individuos-tot_padres]
+    #print("tot poblacion de mejores: " , len(poblacion))
+
+    ##Seleccion de los padres que seran cruzados
+    for i in range(tot_padres):
+        indexPadre1 = rnd.randint(0, tot_padres-1) #aleatorio entre 0 y n-1
+        indexPadre2 = rnd.randint(0, tot_padres-1) #aleatorio entre 0 y n-1
+        while(indexPadre1==indexPadre2):
+            indexPadre2 = rnd.randint(0, tot_padres - 1)  # aleatorio entre 0 y n-1
+
+        tempPadre1 = poblacion[indexPadre1]
+        tempPadre2 = poblacion[indexPadre2]
+
+        #print(tempPadre1)
+        #print(tempPadre2)
+
+        if tempPadre1[1] >= tempPadre2[1]:
+            padres.append(tempPadre1[0].copy())
+        else:
+            padres.append(tempPadre2[0].copy())
+
+    #print("Padres para cruza: ")
+    #for index, padre in enumerate(padres):
+    #    print(index,".-", padre)
+
+    hijos = []
+    for i in range(0,tot_padres, 2):
+        tempPadre1 = padres[i]
+        tempPadre2 = padres[i+1]
+
+        #Generar un aleatorio
+        puntoCruza = rnd.randint(0, tot_genes-1)
+
+        #La primera parte del padre 1, será la primera parte del hijo 1,
+        # la segunda parte del padre 1, será la segunda parte del hijo 2
+
+        # La primera parte del padre 2, será la primera parte del hijo 2,
+        # la segunda parte del padre 2, será la segunda parte del hijo 1
+
+        # Generar un aleatorio
+        puntoCruza += 1  # +1 -> puntoCruza incluyente
+        hijo1 = tempPadre1[:puntoCruza] + tempPadre2[puntoCruza:]
+        hijo2 = tempPadre2[:puntoCruza] + tempPadre1[puntoCruza:]
+
+        hijos.append([hijo1, 0])
+        hijos.append([hijo2, 0])
+
+
+    #Mutacion
+    probMuta = 0.9
+    for indexHijo in range(len(hijos)):
+        hijo = hijos[indexHijo][0]
+
+        for indexGen in range(len(hijo)):
+            r = rnd.random() # 0 - 1
+            if r >= probMuta:
+                #se efectua la mutacion
+                rnd.uniform(0,0.4)
+                val = rnd.uniform(0, 0.5) if rnd.random() >= 0.5 else  rnd.uniform(0.6, 1)
+
+                val *= hijo[int(indexGen)]
+
+                #print(val)
+
+                #if hijo[indexGen] != val:
+                #    pass
+
+                hijo[indexGen] = val
+
+        hijos[indexHijo][1] = calc_FO_Sphere(hijo)
+
+    ##poblacion completa
+    #mejores individuos + hijos
+    poblacion += hijos
+
+
+
+    print("Mejor Solucion Actual:" , mejorActual)
+
--- Programas/KNN/P1_KNN.py
+++ Programas/KNN/P1_KNN.py
@@ -6,7 +6,11 @@ from MetricasSimilitud import EuclidianaPro, Canberra, Manhattan, Euclidiana
 
 ###CARGAR INSTANCIA DE ENTRENAMIENTO
 
-archivo = open("wine_entrenamiento7525.csv","r")
+#archivo = open("wine_training60.0.csv","r")
+#archivo = open("wine_training70.0.csv","r")
+#archivo = open("wine_training80.0.csv","r")
+archivo = open("wine_training90.0.csv","r")
+
 contenido = archivo.readlines()
 
 #VISUALIZA EL CONTENIDO DEL ARCHIVO
@@ -24,10 +28,11 @@ lista = [linea.split(",") for linea in contenido]
 #VISUALIZA LISTA PROCESADA
 print("Lista de listas separadas por comas: ")
 #Impreso línea a línea
+"""
 for l in lista:
     print(l)
 print("\n\n")
-
+"""
 
 instancia = [ [ list(map(float,x[:13])), float(x[13].replace("\n","")) ] for x in lista ] #iris
 
@@ -36,33 +41,39 @@ print("Total de datos de la Instancia",len(instancia))
 print("Instancia de entrenamiento:")
 #VISUALIZA EL CONTENIDO DEL ARCHIVO
 #Impreso línea a línea
+"""
 for l in instancia:
     print(l)
 print("\n\n")
-
+"""
 
 ##############################################################################
 ###CARGAR INSTANCIA DE PRUEBA
 
-archivo = open("wine_prueba7525.csv","r")
+#archivo = open("wine_test60.0.csv","r")
+#archivo = open("wine_test70.0.csv","r")
+#archivo = open("wine_test80.0.csv","r")
+archivo = open("wine_test90.0.csv","r")
 contenido = archivo.readlines()
 
 #VISUALIZA EL CONTENIDO DEL ARCHIVO
+"""
 print('\nArchivo Completo: ') #Impreso línea a línea
 for l in contenido:
     print(l, end="") #por el formato en que se lee el archivo se quita el terminador (salto de linea) para evidar un doble salto
 print("\n\n")
-
+"""
 
 lista = [linea.split(",") for linea in contenido]
 
 #VISUALIZA LISTA PROCESADA
 print("Lista de listas separadas por comas: ")
 #Impreso línea a línea
+"""
 for l in lista:
     print(l)
 print("\n\n")
-
+"""
 prueba = [ [ list(map(float,x[:13])), float(x[13].replace("\n","")) ] for x in lista ] #iris
 
 print("Total de datos de la Instancia",len(prueba))
@@ -70,10 +81,11 @@ print("Total de datos de la Instancia",len(prueba))
 print("Instancia de prueba:")
 #VISUALIZA EL CONTENIDO DEL ARCHIVO
 #Impreso línea a línea
+"""
 for l in prueba:
     print(l)
 print("\n\n")
-
+"""
 
 ###############################################################################
 ###DEFINIR QUE MEDIDA DE SIMILITUD SE UTILIZARÁ
@@ -122,7 +134,7 @@ for K in range(1,len(instancia)):
             #print(distancia_NC_i)
             estructuraDatos[NoCaso] = distancia_NC_i
 
-        #ordenado = sorted(estructuraDatos.items(), key=lambda x: x[1])
+
         ordenado = sorted(estructuraDatos.items(), key=lambda x: x[1], reverse = False)
 
 
@@ -161,12 +173,6 @@ for K in range(1,len(instancia)):
     print("Total de pruebas: " + str(len(prueba)))
     print("Rendimiento: " + str(contAciertos/len(prueba)*100))
 
-
-"""
-    if contAciertos == len(prueba) and K != 1:
-        print(f"LA K = {K} PROPORCIONO LOS MEJORES RESULTADOS")
-        break
-"""
 
 print(aciertosK)
 may = 0
